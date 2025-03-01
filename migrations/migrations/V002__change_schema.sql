bca49a7a7e462ba13951705bd816d571c838cc6a
Kirill Pudov <kereeell@Kirills-MacBook-Pro.local>
2025-03-01 17:54:53 +0300

    fix second migration

--- migrations/migrations/V002__change_schema.sql
+++ migrations/migrations/V002__change_schema.sql
@@ -1,6 +1,8 @@
 ALTER TABLE product ADD price DOUBLE PRECISION;
 ALTER TABLE orders ADD date_created date;
+DROP TABLE product_info;
+DROP TABLE orders_date;
 
-ALTER TABLE order_product ADD CONSTRAINT products FOREIGN KEY(product_id) REFERENCES product(id);
+ALTER TABLE order_product ADD CONSTRAINT product_id FOREIGN KEY(product_id) REFERENCES product(id);
 
-ALTER TABLE order_product ADD CONSTRAINT orders FOREIGN KEY(order_id) REFERENCES orders(id);
+ALTER TABLE order_product ADD CONSTRAINT orders_id FOREIGN KEY(order_id) REFERENCES orders(id);
